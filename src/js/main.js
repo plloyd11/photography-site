31e75beea3e8b99352f59d97fdac532dc21dcc6b
Peter Lloyd <plloyd11@gmail.com>
2017-08-18 16:19:43 -0400

    Mobile menu structure

--- src/js/main.js
+++ src/js/main.js
@@ -9,18 +9,18 @@ const $grid = $('.grid').imagesLoaded(function() {
 const filterFns = {
     // show if number is greater than 50
     numberGreaterThan50: function() {
-        var number = $(this).find('.number').text();
+        const number = $(this).find('.number').text();
         return parseInt(number, 10) > 50;
     },
     // show if name ends with -ium
     ium: function() {
-        var name = $(this).find('.name').text();
+        const name = $(this).find('.name').text();
         return name.match(/ium$/);
     }
 };
 // bind filter button click
 $('.filter-button-group').on('click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
+    let filterValue = $(this).attr('data-filter');
     // use filterFn if matches value
     filterValue = filterFns[filterValue] || filterValue;
     $grid.isotope({
@@ -29,7 +29,7 @@ $('.filter-button-group').on('click', 'button', function() {
 });
 // change is-checked class on buttons
 $('.button-group').each(function(i, buttonGroup) {
-    var $buttonGroup = $(buttonGroup);
+    const $buttonGroup = $(buttonGroup);
     $buttonGroup.on('click', 'button', function() {
         $buttonGroup.find('.is-checked').removeClass('is-checked');
         $(this).addClass('is-checked');
@@ -41,8 +41,9 @@ lightbox.option({
     'wrapAround': true
 });
 
-(function () {
-	$('.hamburger-menu').on('click', function() {
-		$('.bar').toggleClass('animate');
-	})
-})();
+new Vue ({
+  el: '#nav',
+  data: {
+    mobileMenu: false
+  }
+});
