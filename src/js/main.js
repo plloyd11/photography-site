b41e7a2979f5245e848802eacf7994f95ecbce92
Peter Lloyd <plloyd11@gmail.com>
2017-08-22 15:02:42 -0400

    Mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,47 @@
 const $grid = $('.grid').imagesLoaded(function() {
-    // init Isotope after all images have loaded
-    $grid.isotope({
-        itemSelector: '.grid-item',
-        percentPosition: true
-    });
+  // init Isotope after all images have loaded
+  $grid.isotope({
+    itemSelector: '.grid-item',
+    percentPosition: true
+  });
 });
 // filter functions
 const filterFns = {
-    // show if number is greater than 50
-    numberGreaterThan50: function() {
-        const number = $(this).find('.number').text();
-        return parseInt(number, 10) > 50;
-    },
-    // show if name ends with -ium
-    ium: function() {
-        const name = $(this).find('.name').text();
-        return name.match(/ium$/);
-    }
+  // show if number is greater than 50
+  numberGreaterThan50: function() {
+    const number = $(this).find('.number').text();
+    return parseInt(number, 10) > 50;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    const name = $(this).find('.name').text();
+    return name.match(/ium$/);
+  }
 };
 // bind filter button click
 $('.filter-button-group').on('click', 'button', function() {
-    let filterValue = $(this).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[filterValue] || filterValue;
-    $grid.isotope({
-        filter: filterValue
-    });
+  let filterValue = $(this).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[filterValue] || filterValue;
+  $grid.isotope({
+    filter: filterValue
+  });
 });
 // change is-checked class on buttons
 $('.button-group').each(function(i, buttonGroup) {
-    const $buttonGroup = $(buttonGroup);
-    $buttonGroup.on('click', 'button', function() {
-        $buttonGroup.find('.is-checked').removeClass('is-checked');
-        $(this).addClass('is-checked');
-    });
+  const $buttonGroup = $(buttonGroup);
+  $buttonGroup.on('click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $(this).addClass('is-checked');
+  });
 });
 
 lightbox.option({
-    'resizeDuration': 200,
-    'wrapAround': true
+  'resizeDuration': 200,
+  'wrapAround': true
 });
 
-new Vue ({
+new Vue({
   el: '#nav',
   data: {
     mobileMenu: false
